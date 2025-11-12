6bf12fdeeb430e289a7b44b5ae98a275908d771a
Maximilian6741 <mc_mkirsh27@mcjvs2.org>
2025-11-12 08:54:37 -0500

    first commit

---
+++ js_1/about-me.js
@@ -0,0 +1,19 @@
+//Maximilian
+
+//Display pop up message
+alert('Welcome!');
+
+//Display heading to page
+document.write('<h1>Maximilian Kirsh</h1>');
+
+//Display paragraphs to page
+document.write('<p>My name is Max, I am 17 years old and I am attending MCCC to persue a career in Software Engineering and Web Development! I was born in Cleveland Ohio in 2008. I grew up in Brunswick Ohio with both my parents, and 4 siblings. I have two cats and a dog named Hank. I played for the Brunswick Blue Devils freshman basketball team. I have a beautiful girlfriend name Alexis who is also attending MCCC! I like playing the Guitaarrrr, fishing, driving, hanging out with my girlfriend, and playing with my dog.');
+
+//Display message in console
+console.log('Hello!')
+
+//Display message in console
+console.log('This is the console!')
+
+//Display message in console
+console.log('Pretty coooool aint it?')
---
+++ js_1/script.js
@@ -0,0 +1,6 @@
+//Maximilian Kirsh
+
+//Display message in console
+console.log('External file loaded!');
+
+document.write('<p>Today I learned how to create messages in Javascript. I can now use console.log to Display messages in the console. And I can now disaplay messages on the page using JavaScript.')
